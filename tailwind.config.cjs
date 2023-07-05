bfa6776a92923c6e2449ffdaea6e7f483ce320e9
Jorge Luis Hernández Dueñas <jorgelhd94@gmail.com>
2023-07-05 15:23:08 -0400

    fix theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,6 +11,6 @@ module.exports = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["light", "dark"],
+    themes: ["light", "dracula"],
   },
 };
